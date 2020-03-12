ad71d7baedff10e309cccb588610d5e0dfea7283
Julie Carra <julie.carra@live.fr>
2020-03-12 23:00:51 +0100

    Fixing database for deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
